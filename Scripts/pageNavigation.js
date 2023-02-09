e27577546d8847cf3280418e39b8ac5a60702b0d
archieforster <archief@btinternet.com>
2023-02-09 17:45:10 +0000

    Page Navigation

---
+++ Scripts/pageNavigation.js
@@ -0,0 +1,20 @@
+let pageStack = []
+
+function RedirectTo(inputPagename){
+    // Backbutton implementation
+    if (inputPageName == "BACK"){
+        // Get previous page from stack
+        pagename = pageStack.pop()
+        // Redirect to page if exists
+        if (pagename != undefined){
+            window.location.pathname = "/" + pagename
+        }
+
+    }else{
+        //Adds pagename to stack
+        pageStack.push(inputPagename)
+        //Redirects to page
+        window.location.pathname = "/" + pagename   
+    }
+    
+}
