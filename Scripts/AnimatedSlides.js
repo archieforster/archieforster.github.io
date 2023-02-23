65fe43dc180e5a931b35407b3b1b820c3402baf6
archieforster <archief@btinternet.com>
2023-02-23 16:47:59 +0000

    Edited Nav Bar

---
+++ Scripts/AnimatedSlides.js
@@ -0,0 +1,27 @@
+let slideIndex = 0
+let slides = document.getElementsByClassName("about-me-slides-images")
+
+
+function ShowNextSlide(){
+    // Calculate next slide index using Modulo
+    nextSlideIndex = (slideIndex + 1) % slides.length
+
+    // Remove previous slide and display next one
+    slides[slideIndex].style.display = "none"
+    slides[nextSlideIndex].style.display = "block"
+
+    // Set current slide index to new index
+    slideIndex = nextSlideIndex
+}
+
+function ShowPreviousSlide(){
+    // Calculate next slide index using Modulo
+    prevSlideIndex = (slideIndex - 1) % slides.length
+
+    // Remove previous slide and display next one
+    slides[slideIndex].style.display = "none"
+    slides[prevSlideIndex].style.display = "block"
+
+    // Set current slide index to new index
+    slideIndex = prevSlideIndex
+}
